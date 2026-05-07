11524706f2b03ad27f5982ff6220075cbccbe230
naujtrats <120572560+chickenyoutoo-beautiful@users.noreply.github.com>
2026-05-07 21:13:48 +0800

    Add 刷课/api/logger.py

---
+++ api/logger.py
@@ -0,0 +1,15 @@
+import os, sys, time as _time
+from loguru import logger
+
+log_dir = '/tmp/AutomaticCB'
+log_path = os.path.join(log_dir, 'chaoxing.log')
+try:
+    if os.path.exists(log_path) and not os.access(log_path, os.W_OK):
+        log_path = os.path.join(log_dir, 'chaoxing_%s.log' % _time.strftime('%Y-%m-%d_%H-%M-%S'))
+    logger.add(log_path, rotation='10 MB', level='TRACE',
+                format='{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}')
+except Exception:
+    try:
+        logger.add(sys.stderr, level='DEBUG')
+    except Exception:
+        pass
