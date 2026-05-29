0cb65ab2e4288ad2c1b15cb667801108bfea5316
naujtrats <naujtrats@gmail.com>
2026-05-30 01:34:20 +0800

    fix: ReferenceError - 'result' variable name mismatch in _renderMarkdownWithMath
    - _restoreMath was accidentally removed when adding auto-linkify
    - Fixed: var tempHtml = _restoreMath(html); then operate on tempHtml

--- js/main.js
+++ js/main.js
@@ -102,14 +102,15 @@ function _renderMarkdownWithMath(text) {
     const protected = _protectMath(text);
     const html = marked.parse(protected);
     // ★ 自动将纯文本 URL 转为可点击链接（marked v15 不自动 linkify）
-    result = result.replace(/(?<!["'=])(https?:\/\/[^\s<>"']+)(?!["'])/gi, function(url) {
+    var tempHtml = _restoreMath(html);
+    tempHtml = tempHtml.replace(/(?<!["'=])(https?:\/\/[^\s<>"']+)(?!["'])/gi, function(url) {
         // 清理尾部标点
         var cleanUrl = url.replace(/[.,;:!?)\]]+$/, '');
         return '<a href="' + cleanUrl + '" target="_blank" rel="noopener">' + cleanUrl + '</a>';
     });
     // ★ 所有已经存在的链接打开新标签页
-    result = result.replace(/<a /g, '<a target="_blank" rel="noopener" ');
-    return result;
+    tempHtml = tempHtml.replace(/<a /g, '<a target="_blank" rel="noopener" ');
+    return tempHtml;
 }
 
 // 一键修复配置
